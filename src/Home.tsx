84f1a80427956315b6bbeb8c6f53bbbbe9a61fb3
sodie-oxige <sodie.oxige@gmail.com>
2026-03-11 19:39:45 +0900

    イラストと3dモデルの表示を分割

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,18 +1,15 @@
+import { useState } from "react";
+
 import Icon from "./assets/icon.png";
-import AntooThumbnail from "./assets/image/Antoo-thumbnail01.png";
 import HiddenLabelButton from "./component/HiddenLabelButton";
+import ThreeDimensional from "./Work/3DCG";
+import Illust from "./Work/Illust";
 
 const SnsIconsGlob = import.meta.glob<{ default: string }>(
   "./assets/sns/*.{png,jpg,jpeg,svg,gif}",
   { eager: true },
 );
 const SnsIcons: Record<string, string> = toImagePaths(SnsIconsGlob);
-
-const IllustGlob = import.meta.glob<{ default: string }>(
-  "./assets/illust/*.{png,jpg,jpeg,svg,gif}",
-  { eager: true },
-);
-const Illust: Record<string, string> = toImagePaths(IllustGlob);
 
 function toImagePaths(glob: Record<string, { default: string }>) {
   const res: Record<string, string> = {};
@@ -26,6 +23,8 @@ function toImagePaths(glob: Record<string, { default: string }>) {
 }
 
 function Home() {
+  const [articleIndex, setArticleIndex] = useState(0);
+
   return (
     <>
       <title>袖置き場 - Home</title>
@@ -111,131 +110,45 @@ function Home() {
           </div>
         </div>
 
-        <div className="w-full">
-          <span className="font-goldman block translate-y-2 text-xl">
-            ILLUSTLATION
-          </span>
-          <div className="flex w-full flex-col gap-8 border p-4 md:px-12 md:py-8">
-            <div className="flex w-full flex-row flex-wrap items-center justify-center">
-              <img
-                src={Illust["2025-12_結月ゆかり"]}
-                className="max-h-[70vh] rounded-xl shadow-lg"
-              />
-              <div className="m-4 flex-1 md:mx-12 md:my-8">
-                <h2 className="mb-4 text-2xl font-bold">
-                  結月ゆかり
-                  <span className="text-xs"> - vocaloid/voiceloid</span>
-                </h2>
-                <p>
-                  羽賀氏の合成音声イラスト集『synthetic
-                  singers!』に寄稿したイラストです。
-                </p>
-              </div>
-            </div>
-            <hr />
-
-            <div className="flex w-full flex-row-reverse flex-wrap items-center justify-center">
-              <img
-                src={Illust["2025-12_結月ゆかり2"]}
-                className="max-h-[70vh] rounded-xl shadow-lg"
-              />
-              <div className="m-4 flex-1 md:mx-12 md:my-8">
-                <h2 className="mb-4 text-2xl font-bold">
-                  結月ゆかり
-                  <span className="text-xs"> - vocaloid/voiceloid</span>
-                </h2>
-                <p>
-                  羽賀氏の合成音声イラスト集『synthetic
-                  singers!』に寄稿したイラストです。
-                </p>
-              </div>
-            </div>
-            <hr />
-
-            <div className="flex w-full flex-row flex-wrap items-center justify-center">
-              <img
-                src={Illust["2025-12_ハイモア"]}
-                className="max-h-[70vh] rounded-xl shadow-lg"
-              />
-              <div className="m-4 flex-1 md:mx-12 md:my-8">
-                <h2 className="mb-4 text-2xl font-bold">
-                  ハイモア
-                  <span className="text-xs"> - アークナイツ</span>
-                </h2>
-                <p>
-                  浅井ガミ氏のアークナイツイラスト集『Azure』に寄稿したイラストです。
-                </p>
-              </div>
-            </div>
-            <hr />
-
-            <div className="flex w-full flex-row-reverse flex-wrap items-center justify-center">
-              <img
-                src={Illust["2025-12_ミュルジス"]}
-                className="max-h-[70vh] rounded-xl shadow-lg"
-              />
-              <div className="x-4 flex-1 md:mx-12 md:my-8">
-                <h2 className="mb-4 text-2xl font-bold">
-                  ミュルジス
-                  <span className="text-xs"> - アークナイツ</span>
-                </h2>
-                <p>
-                  よか氏のアークナイツイラスト集『THERMO』に寄稿したイラストです。
-                </p>
-              </div>
-            </div>
+        <div className="font-goldman flex flex-col gap-4 underline">
+          <MarkedElement isMarked={articleIndex == 0}>
+            <span onClick={() => setArticleIndex(0)} className="text-2xl">
+              WORK
+            </span>
+          </MarkedElement>
+          <div className="ml-4 flex gap-12 text-lg">
+            <MarkedElement isMarked={articleIndex == 1}>
+              <span onClick={() => setArticleIndex(1)}>ILLUSTLATION</span>
+            </MarkedElement>
+            <MarkedElement isMarked={articleIndex == 2}>
+              <span onClick={() => setArticleIndex(2)}>3D MODEL</span>
+            </MarkedElement>
           </div>
         </div>
-        <div className="w-full">
-          <span className="font-goldman block translate-y-2 text-xl">
-            3D MODEL
-          </span>
-          <div className="flex w-full flex-col gap-8 border p-4 md:px-12 md:py-8">
-            <div className="flex w-full flex-row flex-wrap items-center justify-center">
-              <img
-                flex-wrap
-                justify-center
-                src={AntooThumbnail}
-                className="max-h-[50vh] rounded-xl shadow-lg"
-              />
-              <div className="x-4 flex-1 md:mx-12 md:my-8">
-                <h2 className="mb-4 text-2xl font-bold">
-                  オリジナル3Dモデル『アントー』
-                  <span className="text-xs">¥1,000</span>
-                </h2>
-                <p>VRChatでの使用を想定したオリジナル3dモデルです。</p>
-                <ul className="my-4 list-disc pl-6">
-                  <li>ポリゴン数 △29,668</li>
-                  <li>liltoonシェーダー使用</li>
-                  <li>
-                    VCC用unitypackageファイル、3dモデルblendファイル、
-                    <br />
-                    テクスチャpsdファイルあり
-                  </li>
-                  <li>
-                    マテリアルは　身体,衣装,メガネレンズの3種。
-                    <br />
-                    衣装は複数パターン、各マテリアルはmobile版も同梱
-                  </li>
-                  <li>両手14種（右手優先）で表情設定済み</li>
-                  <li>
-                    エクスプレッションメニューからの衣装の着脱対応
-                    <br />
-                    （excellent版でも利用可！）
-                  </li>
-                </ul>
-                <a
-                  href="https://sodie-oxige.booth.pm/items/7620978"
-                  className="text-blue-500 underline"
-                >
-                  商品ページ↗
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+
+        {[0, 1].includes(articleIndex) && <Illust />}
+        {[0, 2].includes(articleIndex) && <ThreeDimensional />}
       </div>
     </>
+  );
+}
+
+function MarkedElement({
+  children,
+  isMarked,
+}: {
+  children: React.ReactElement;
+  isMarked: boolean;
+}) {
+  return (
+    <div className="relative">
+      {children}
+      {isMarked && (
+        <div className="absolute top-0 bottom-0 -left-3 flex items-center">
+          <div className="border-y-3 border-l-6 border-x-black border-y-transparent"></div>
+        </div>
+      )}
+    </div>
   );
 }
 
---
+++ src/Work/3DCG.tsx
@@ -0,0 +1,54 @@
+import AntooThumbnail from "../assets/image/Antoo-thumbnail01.png";
+
+function ThreeDimensional() {
+  return (
+    <div className="w-full">
+      <span className="font-goldman block translate-y-2 text-xl">3D MODEL</span>
+      <div className="flex w-full flex-col gap-8 border p-4 md:px-12 md:py-8">
+        <div className="flex w-full flex-row flex-wrap items-center justify-center">
+          <img
+            flex-wrap
+            justify-center
+            src={AntooThumbnail}
+            className="max-h-[50vh] rounded-xl shadow-lg"
+          />
+          <div className="x-4 flex-1 md:mx-12 md:my-8">
+            <h2 className="mb-4 text-2xl font-bold">
+              オリジナル3Dモデル『アントー』
+              <span className="text-xs">¥1,000</span>
+            </h2>
+            <p>VRChatでの使用を想定したオリジナル3dモデルです。</p>
+            <ul className="my-4 list-disc pl-6">
+              <li>ポリゴン数 △29,668</li>
+              <li>liltoonシェーダー使用</li>
+              <li>
+                VCC用unitypackageファイル、3dモデルblendファイル、
+                <br />
+                テクスチャpsdファイルあり
+              </li>
+              <li>
+                マテリアルは　身体,衣装,メガネレンズの3種。
+                <br />
+                衣装は複数パターン、各マテリアルはmobile版も同梱
+              </li>
+              <li>両手14種（右手優先）で表情設定済み</li>
+              <li>
+                エクスプレッションメニューからの衣装の着脱対応
+                <br />
+                （excellent版でも利用可！）
+              </li>
+            </ul>
+            <a
+              href="https://sodie-oxige.booth.pm/items/7620978"
+              className="text-blue-500 underline"
+            >
+              商品ページ↗
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ThreeDimensional;
